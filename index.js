6b742c29e0245a38a0b1a902901867c1f1716dd3
spikeburton <ralphburton3@gmail.com>
2019-03-09 11:12:07 -0500

    Done.

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 let currentUser = "codedaddy";
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = `Welcome, ${currentUser[0]}!`
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
